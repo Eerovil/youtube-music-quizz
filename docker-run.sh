9fcbf7f8f2fc82dd2010638fa0cb29fce138a656
Eero Vilpponen <eero.vilpponen@gmail.com>
2024-03-25 18:26:44 +0200

    Add network

--- docker-run.sh
+++ docker-run.sh
@@ -14,4 +14,4 @@ docker build -t youtube-music-quiz:$ENV .
 echo "Running container with branch: $ENV"
 
 # Use hostname alias youtube-music-quiz-$ENV.
-docker run -it youtube-music-quiz:$ENV --name youtube-music-quiz-$ENV --network tunnel
+docker run --network tunnel -it youtube-music-quiz:$ENV --name youtube-music-quiz-$ENV

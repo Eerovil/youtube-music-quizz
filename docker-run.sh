f20c543209af8ac09737950d46478fc5e1893b84
Eero Vilpponen <eero.vilpponen@gmail.com>
2024-03-27 09:24:25 +0000

    Add rm container

--- docker-run.sh
+++ docker-run.sh
@@ -13,5 +13,8 @@ docker build -t youtube-music-quiz:$ENV .
 # Run the container
 echo "Running container with branch: $ENV"
 
+# Remove any existing container with the same name.
+docker rm -f youtube-music-quiz-$ENV
+
 # Use hostname alias youtube-music-quiz-$ENV.
 docker run --rm --network tunnel --name youtube-music-quiz-$ENV -it youtube-music-quiz:$ENV

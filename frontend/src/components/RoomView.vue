0e0c9244a71b1b5d93cfa037812461b6922c26d6
Eero Vilpponen <eero.vilpponen@gmail.com>
2024-03-29 17:15:19 +0000

    Changes

---
+++ frontend/src/components/RoomView.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="hello">
+        <h1>{{ title }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'RoomView',
+    props: {
+    },
+    data: function() {
+    },
+    computed: {
+        title() {
+            return (this.$store.state.room || {}).title;
+        },
+    },
+    mounted() {
+        this.getAudioStream();
+    },
+    methods: {
+        async getAudioStream() {
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
